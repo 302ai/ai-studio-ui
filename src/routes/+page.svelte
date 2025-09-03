7f97c6ba4d1fb910b8851911f9e0340483d8a653
mthezi <mthezi@163.com>
2025-09-03 17:08:55 +0800

    add theme switcher

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Button from "@/components/ui/button/button.svelte";
+	import ThemeSwitcher from "@/components/ui/theme-switcher.svelte";
 
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <Button>Click me</Button>
+
+<ThemeSwitcher />
