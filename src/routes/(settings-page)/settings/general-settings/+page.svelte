6daf5a7c1828f9ae49fa917ed779a0eab39c0a94
shiwo6324 <103474153+shiwo6324@users.noreply.github.com>
2025-09-04 17:58:36 +0800

    feat(settings): add layout mode selection and update language options

--- src/routes/(settings-page)/settings/general-settings/+page.svelte
+++ src/routes/(settings-page)/settings/general-settings/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Language from './language.svelte';
+	import LayoutMode from './layout-mode.svelte';
 </script>
 
 <Language />
+<LayoutMode />
---
+++ src/routes/(settings-page)/settings/general-settings/layout-mode copy.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import * as Select from '$lib/components/ui/select/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { m } from '$lib/paraglide/messages.js';
+
+	const languages = [
+		{
+			key: 'default',
+			label: '默认',
+			value: 'default'
+		},
+		{
+			key: 'wide',
+			label: '宽',
+			value: 'wide'
+		},
+		{
+			key: 'ultraWide',
+			label: '超宽',
+			value: 'ultra-wide'
+		}
+	];
+
+	let value = $state('default');
+
+	function getLabel(val: string) {
+		return languages.find((l) => l.value === val)?.label || val;
+	}
+</script>
+
+<Label for="layout" class="text-label-fg">{m.layout()}</Label>
+
+<Select.Root type="single" name="layout" bind:value>
+	<Select.Trigger class="w-full">{getLabel(value)}</Select.Trigger>
+	<Select.Content>
+		{#each languages as language (language.key)}
+			<Select.Item value={language.value} label={language.label} />
+		{/each}
+	</Select.Content>
+</Select.Root>
