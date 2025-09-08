bfbe7ab57b14c447652e9cd56427c6b9a5ca3e66
shiwo6324 <103474153+shiwo6324@users.noreply.github.com>
2025-09-08 11:31:20 +0800

    feat(stream-settings): add stream output speed options and UI enhancements
    - Introduced speed options (slow, normal, fast) for stream output settings with corresponding icons.
    - Updated the stream-setting component to conditionally display speed options based on the auto stream output toggle.
    - Enhanced user interface for better interaction and clarity in settings.

--- src/routes/(settings-page)/settings/(center)/preferences-settings/stream-setting.svelte
+++ src/routes/(settings-page)/settings/(center)/preferences-settings/stream-setting.svelte
@@ -1,12 +1,47 @@
 <script lang="ts">
-	import { SettingSwitchItem } from "$lib/components/buss/settings";
+	import { SegButton, SettingSwitchItem } from "$lib/components/buss/settings";
 	import { Label } from "$lib/components/ui/label/index.js";
 	import { m } from "$lib/paraglide/messages.js";
+	import { Rabbit, Timer, Zap } from "@lucide/svelte";
 
 	let autoStreamOutput = $state(false);
+	let speed = $state("normal");
+
+	const speedOptions = [
+		{
+			key: "slow",
+			label: m.settings_slow(),
+			icon: Rabbit,
+			iconSize: 16,
+		},
+		{
+			key: "normal",
+			label: m.settings_normal(),
+			icon: Timer,
+			iconSize: 16,
+		},
+		{
+			key: "fast",
+			label: m.settings_fast(),
+			icon: Zap,
+			iconSize: 16,
+		},
+	];
+
+	function handleSelect(key: string) {
+		speed = key;
+	}
 </script>
 
-<div class="flex flex-col gap-2">
-	<Label class="text-label-fg">{m.settings_streamOutputLabel()}</Label>
-	<SettingSwitchItem label={m.settings_streamOutputEnable()} bind:checked={autoStreamOutput} />
+<div class="flex flex-col gap-3">
+	<div class="space-y-2">
+		<Label class="text-label-fg">{m.settings_streamOutputLabel()}</Label>
+		<SettingSwitchItem label={m.settings_streamOutputEnable()} bind:checked={autoStreamOutput} />
+	</div>
+	{#if autoStreamOutput}
+		<div class="space-y-2">
+			<Label class="text-label-fg">{m.settings_streamOutputSpeed()}</Label>
+			<SegButton options={speedOptions} selectedKey={speed} onSelect={handleSelect} />
+		</div>
+	{/if}
 </div>
